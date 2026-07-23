ba6022b7bc33989326d6b091468da45b7b5937ef
TheJaiSingh <sjai4247@gmail.com>
2026-07-23 23:13:41 +0530

    Time: 547 ms (25.25%) | Memory: 12.5 MB (29.19%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,31 +1,28 @@
 class Solution(object):
     def fourSum(self, nums, target):
         arr=sorted(nums)
-        best=[]
+        bestpair=[]
         for i in range(len(arr)-3):
             if i>0 and arr[i]==arr[i-1]:
                 continue
             for j in range(i+1,len(arr)-2):
                 if j>i+1 and arr[j]==arr[j-1]:
-                    continue
+                    continue    
                 left=j+1
                 right=len(arr)-1
 
                 while left<right:
                     currentsum=arr[i]+arr[j]+arr[left]+arr[right]
                     if currentsum==target:
-                        best.append([arr[i],arr[j],arr[left],arr[right]])
+                        bestpair.append([arr[i],arr[j],arr[left],arr[right]])
                         left+=1
                         right-=1
-                        
                         while left<right and arr[left]==arr[left-1]:
                             left+=1
                         while left<right and arr[right]==arr[right+1]:
                             right-=1
-                        
                     elif currentsum<target:
                         left+=1
                     else:
                         right-=1
-        return best
-        
+        return bestpair
