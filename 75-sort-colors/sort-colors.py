63f22622aa2866caf2fabf058d5fd4aa00049e2c
TheJaiSingh <sjai4247@gmail.com>
2026-08-14 09:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.88%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortColors(self, nums):
+        low=0
+        i=0
+        high=len(nums)-1
+        while i<=high:
+            if nums[i]==0:
+                nums[i],nums[low]=nums[low],nums[i]
+                i+=1
+                low+=1
+            elif nums[i]==1:
+                i+=1
+            else:
+                nums[i],nums[high]=nums[high],nums[i]
+                high-=1
