4f1760ce17c66f2b5973fa6ade97a81158950257
TheJaiSingh <sjai4247@gmail.com>
2026-08-19 08:26:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (87.81%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        maxx=max(arr1)
+        count=[0]*(maxx+1)
+        for num in arr1:
+            count[num]+=1
+        result=[]
+
+        for num in arr2:
+            while count[num]>0:
+                result.append(num)
+                count[num]-=1
+        for c in range(maxx+1):
+            while count[c]>0:
+                result.append(c)
+                count[c]-=1
+        return result
