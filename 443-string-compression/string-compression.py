03915cdab745edc14bdacc48f602e1e173d75c6e
TheJaiSingh <sjai4247@gmail.com>
2026-08-30 23:01:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (16.33%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,28 +1,21 @@
 class Solution(object):
     def compress(self, chars):
-
-        count = 1
-        result = ""
-
-        for i in range(len(chars) - 1):
-
-            if chars[i] == chars[i + 1]:
-                count += 1
-
+        result=""
+        count=1
+        for i in range(len(chars)-1):
+            if chars[i]==chars[i+1]:
+                count+=1
             else:
-                result += chars[i]
+                result+=chars[i]
+                if count>1:
+                    result+=str(count)
 
-                if count > 1:
-                    result += str(count)
-
-                count = 1
-
-        result += chars[-1]
-
-        if count > 1:
-            result += str(count)
-
+                count=1
+        result+=chars[-1]
+        if count>1:
+            result+=str(count)
         for i in range(len(result)):
-            chars[i] = result[i]
-
-        return len(result)
+            chars[i]=result[i]
+        return len(result)        
+        
+        
