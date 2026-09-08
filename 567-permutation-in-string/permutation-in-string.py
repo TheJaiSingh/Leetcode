02fba3341b25efd938ecefdbffdbac6c4beaf52a
TheJaiSingh <sjai4247@gmail.com>
2026-09-09 02:00:50 +0530

    Time: 26 ms (57.34%) | Memory: 12.7 MB (78.13%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        freq = {}
+        for i in range(len(s1)):
+            if s1[i] not in freq:
+                freq[s1[i]] = 1
+            else:
+                freq[s1[i]] += 1
+
+        # sliding window over s2
+        window = {}
+        left = 0
+        for right in range(len(s2)):
+            if s2[right] not in window:
+                window[s2[right]] = 1
+            else:
+                window[s2[right]] += 1
+
+            # shrink window if size exceeds len(s1)
+            if right - left + 1 > len(s1):
+                window[s2[left]] -= 1
+                if window[s2[left]] == 0:
+                    del window[s2[left]]
+                left += 1
+
+            # check if current window matches freq
+            if window == freq:
+                return True
+
+        return False
