37513931f6cade1480778c660f3ae86bfbf0a69d
TheJaiSingh <sjai4247@gmail.com>
2026-09-15 23:55:43 +0530

    Time: 93 ms (37.10%) | Memory: 19.1 MB (51.79%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,13 +1,24 @@
 class Solution(object):
     def findMaxAverage(self, nums, k):
-        windowsum=0
-        maxsum=float("-inf")
-
-        for right in range(len(nums)):
-            windowsum+=nums[right]
-            if right>=k-1:
-                maxsum=max(windowsum,maxsum)
-                windowsum-=nums[right-k+1]
-        return float(maxsum)/k
-
-
+        
+        # First window
+        window_sum = 0
+        
+        for i in range(k):
+            window_sum += nums[i]
+        
+        max_sum = window_sum
+        
+        left = 0
+        
+        # Slide the window
+        for right in range(k, len(nums)):
+            
+            window_sum -= nums[left]
+            window_sum += nums[right]
+            
+            left += 1
+            
+            max_sum = max(max_sum, window_sum)
+        
+        return float(max_sum) / k
