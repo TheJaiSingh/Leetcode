824fb41b5659bdbcc33eb47a22a5899f6b48e46a
TheJaiSingh <sjai4247@gmail.com>
2026-09-24 11:07:37 +0530

    Time: 181 ms (95.77%) | Memory: 17.2 MB (32.15%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,18 +1,19 @@
 class Solution(object):
     def totalFruit(self, fruits):
         left=0
-        maxsum=float("-inf")
         freq={}
-        k=2
+        maxlength=0
         for right in range(len(fruits)):
-            freq[fruits[right]]=freq.get(fruits[right],0)+1
-            while len(freq)>k:
-                left_char=fruits[left]
-                freq[left_char]-=1
-                if freq[left_char]==0:
-                    del freq[left_char]
+            if fruits[right] not in freq:
+                freq[fruits[right]]=1
+            else:
+                freq[fruits[right]]+=1
+
+            while len(freq)>2:
+                freq[fruits[left]]-=1
+                if freq[fruits[left]]==0:
+                    del freq[fruits[left]]
                 left+=1
-            current_lenght=right-left+1
-            maxsum=max(current_lenght,maxsum)
-        return maxsum
-                
+            maxlength=max(maxlength,right-left+1)
+
+        return maxlength     
