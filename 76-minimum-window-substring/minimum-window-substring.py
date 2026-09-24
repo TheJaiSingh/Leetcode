2c4d6a9059201814ef3ccd74b2fb7c0b30e35ac5
TheJaiSingh <sjai4247@gmail.com>
2026-09-24 10:42:58 +0530

    Time: 97 ms (61.10%) | Memory: 15.7 MB (24.83%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,66 +1,52 @@
 class Solution(object):
     def minWindow(self, s, t):
 
-        # Frequency of characters required from t
-        need = {}
+        freq = {}
 
         for ch in t:
-            if ch not in need:
-                need[ch] = 1
+            if ch not in freq:
+                freq[ch] = 1
             else:
-                need[ch] += 1
+                freq[ch] += 1
 
-        # Frequency of characters in current window
         window = {}
 
         left = 0
-
-        # Number of required characters satisfied
         formed = 0
 
-        # Number of different required characters
-        required = len(need)
-
-        # Store minimum window
         minlength = float("inf")
         start = 0
 
+        requirement = len(freq)
+
         for right in range(len(s)):
 
-            # Add current character to window
             if s[right] not in window:
                 window[s[right]] = 1
             else:
                 window[s[right]] += 1
 
-            # Check whether this required character is satisfied
-            if s[right] in need:
-                if window[s[right]] == need[s[right]]:
+            if s[right] in freq:
+                if window[s[right]] == freq[s[right]]:
                     formed += 1
 
-            # Window is valid
-            while formed == required:
+            while formed == requirement:
 
-                # Check if current window is smaller
                 if right - left + 1 < minlength:
                     minlength = right - left + 1
                     start = left
 
-                # Remove left character
                 window[s[left]] -= 1
 
-                # If removing it makes the window invalid
-                if s[left] in need:
-                    if window[s[left]] < need[s[left]]:
+                if s[left] in freq:
+                    if window[s[left]] < freq[s[left]]:
                         formed -= 1
 
-                # Delete zero-frequency character
                 if window[s[left]] == 0:
                     del window[s[left]]
 
                 left += 1
 
-        # No valid window found
         if minlength == float("inf"):
             return ""
 
