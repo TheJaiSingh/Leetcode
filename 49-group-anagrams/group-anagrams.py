665f8c474618e60e8d465b77fa31d57beb0c91ce
TheJaiSingh <sjai4247@gmail.com>
2026-09-24 15:24:13 +0530

    Time: 20 ms (61.51%) | Memory: 16.1 MB (56.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,10 +3,8 @@ class Solution(object):
         freq={}
         for i in strs:
             key="".join(sorted(i))
-            if  key not in freq:
+            if key not in freq:
                 freq[key]=[]
             freq[key].append(i)
         return list(freq.values())
-
-
-  
+        
